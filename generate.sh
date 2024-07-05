f0d8c3ff1f3a064a792446b5ade10dd485bf30f1
iorin-io <iodere77@gmail.com>
2024-07-05 21:14:19 +0900

    fix comment

--- generate.sh
+++ generate.sh
@@ -2,17 +2,15 @@
 
 input_xml="data0422.xml"
 
-echo "Extracting item IDs from XML..."
 item_ids=$(xmllint --xpath "//item/@no" $input_xml | sed 's/no="\([0-9]*\)"/\1/g')
-echo "Item IDs extracted: $item_ids"
+echo "item_ids: $item_ids"
 
 for id in $item_ids; do
-    echo "Processing item with ID: $id"
+    echo "item_id: $id"
     xsltproc --stringparam item_id "$id" transform_item.xsl $input_xml > "book-$id.html"
-    echo "Generated HTML for item ID: $id"
 done
 
-echo "All items have been processed."
+echo "done"
 
 #####
 
